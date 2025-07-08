181dbe26426fb53f1c6bce3e916ae12981835b5e
prashant_sonawane <p2352002@gmail.com>
2025-07-08 19:31:01 +0530

    first commit - setup

---
+++ client/src/pages/auth/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Auth = () => {
+  return (
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw]"></div>
+    </div>
+  );
+};
+
+export default Auth;
